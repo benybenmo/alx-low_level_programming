6d2310d7944d746d5c00f4f126af54e80427455f
« Ben <benybenmo@gmail.com>
2023-03-16 09:34:15 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
-#include <studio.h>
+#include <stdio.h>
+
 /**
  * main - A program that prints a line using printf
  * Return: 0 (success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
