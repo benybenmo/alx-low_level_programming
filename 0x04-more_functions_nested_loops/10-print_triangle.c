8fc2cc009264d8f512cfb0e82815cbc5bd169b56
« Ben <benybenmo@gmail.com>
2023-03-23 15:46:40 -0700

    10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,21 +11,21 @@ void print_triangle(int size)
 
 	if (size <= 0)
 	{
-		_putchar('\n');
+	_putchar('\n');
 	}
 	else
 	{
-		for (x = 0; x < size; x++)
-		{
-		for (y = size - x; y > 1; y--)
-		{
-			_putchar(32);
-		}
-		for (z = 0; z - x; z++)
-		{
-			_putchar(35);
-		}
-		_putchar('\n');
-		}
+	for (x = 0; x < size; x++)
+	{
+	for (y = size - x; y > 1; y--)
+	{
+	_putchar(32);
+	}
+	for (z = 0; z <= x; z++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
 	}
 }
